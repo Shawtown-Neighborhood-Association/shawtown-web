4d6382d50ea833b09b8a4c7a96e6e7ca0534d365
Brad Candell <bcandell@pesi.com>
2021-04-06 20:24:11 -0500

    Worked in the addition of the Meeting Agendas with Gatsby's data and node model

--- src/pages/meetings.tsx
+++ src/pages/meetings.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 import { Section } from '../components/Section';
 import { Layout } from '../layout/Layout';
 
-const Meetings = () => {
+
+const Meetings = (props: any) => {
+  const { data } = props;
   return (
     <Layout title="Meetings" description="">
       <>
       <Section>
         <h1>Meetings</h1>
       </Section>
+      <Section>
+        <h1>Agendas</h1>
+        <ul>
+          {data.allMdx.edges.map((x: any, index: number) => {
+            const { node } = x;
+              if (node.fields.source === 'agendas') {
+            return (
+            <li key={index}><Link to={node.fields.slug}>Agenda ({node.frontmatter.starts})</Link></li>
+            )
+            }
+          })}
+        </ul>
+      </Section>
       </>
     </Layout>
   )
 }
 
+export const query = graphql`
+query GetAllMeetingDocuments {
+  allMdx(sort: {fields: frontmatter___starts, order: DESC}) {
+    edges {
+      node {
+        frontmatter {
+          author
+          starts
+          ends
+        }
+        fields {
+          source
+          slug
+        }
+      }
+    }
+  }
+}
+`
 export default Meetings;
--- src/templates/MeetingAgenda.tsx
+++ src/templates/MeetingAgenda.tsx
@@ -1,19 +1,85 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { format } from 'date-fns';
 import { Section } from '../components/Section';
 import Layout from '../layout/Layout';
 
 export default function MeetingAgendaTemplate(props: any) {
-  console.log('Agenda Data', props.data);
+  const { data } = props;
+  const { mdx } = data;
+  const startDate = new Date(mdx.frontmatter.starts);
+  const endDate = new Date(mdx.frontmatter.ends);
 
   return (
     <Layout title="Meeting Agenda" description="">
       <>
         <Section>
-          <h1>Meeting Agenda Template {JSON.stringify(props.data)}</h1>
-          <h2>Brad</h2>
+
+          <h1>Meeting Notice &amp; Agenda</h1>
+          <h2>{mdx.frontmatter.type} Meeting</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Date:</td>
+                <td>{format(startDate, "EEEE, MMMM do, yyyy")}</td>
+              </tr>
+              <tr>
+                <td>Time:</td>
+                <td>{format(startDate, "h:mm aaa")} - {format(endDate, "h:mm aaa")}</td>
+              </tr>
+              <tr>
+                <td>Location:</td>
+                <td>{mdx.frontmatter.location}</td>
+              </tr>
+              <tr>
+                <td colSpan={2}>&nbsp;</td>
+              </tr>
+              <tr>
+                <td>Meeting ID:</td>
+                <td>{mdx.frontmatter.meetingID}</td>
+              </tr>
+              <tr>
+                <td>Passcode:</td>
+                <td>{mdx.frontmatter.meetingPasscode}</td>
+              </tr>
+              <tr>
+                <td>Connect via:</td>
+                <td>Voice / Phone: {mdx.frontmatter.meetingPhone}</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Computer / Smartphone / Tablet<br />{mdx.frontmatter.meetingUrl}</td>
+              </tr>
+            </tbody>
+          </table>
+
+        </Section>
+        <Section>
+          <h3>Meeting Agenda</h3>
+          <MDXRenderer>{mdx.body}</MDXRenderer>
         </Section>
       </>
     </Layout>
   )
 }
 
+export const pageQuery = graphql`
+query GetMeetingAgenda($slug: String!) {
+  mdx(fields: {slug: {eq: $slug}}) {
+    frontmatter {
+      starts
+      ends
+      type
+      location
+      meetingID
+      meetingPasscode
+      meetingPhone
+      meetingUrl
+      author
+    }
+    body
+  }
+}
+
+`
